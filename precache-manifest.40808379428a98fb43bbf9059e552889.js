31fe5153fe6a3fc4a9f8a828b4cf744433ada0b6
VanshikaSingh <vanshika220@gmail.com>
2021-07-03 21:19:52 +0530

    Updates

---
+++ precache-manifest.40808379428a98fb43bbf9059e552889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87b3e26aabd01e4db2578cb028a3dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b47690ae50dc7fe6159",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9eb4caa0d93915eca62f",
+    "url": "/static/js/2.9c4c0123.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.9c4c0123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b47690ae50dc7fe6159",
+    "url": "/static/js/main.3003f50e.chunk.js"
+  },
+  {
+    "revision": "53ff9637c311bd424e1b",
+    "url": "/static/js/runtime-main.5a7750ca.js"
+  },
+  {
+    "revision": "155256543dcede4dcdc70a4b81b2e75f",
+    "url": "/static/media/IMG_1048.15525654.png"
+  }
+]);
