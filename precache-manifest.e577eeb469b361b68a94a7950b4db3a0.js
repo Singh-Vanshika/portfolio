8949444588ca4ace160bc1f6ef48d4a736b7e0c1
VanshikaSingh <vanshika220@gmail.com>
2021-07-03 22:46:31 +0530

    Updates

---
+++ precache-manifest.e577eeb469b361b68a94a7950b4db3a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb96beef8313bfce9e75bba5126551d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "0c991aa7509abcde8ab7",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9eb4caa0d93915eca62f",
+    "url": "/react-deploy/static/js/2.9c4c0123.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/react-deploy/static/js/2.9c4c0123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c991aa7509abcde8ab7",
+    "url": "/react-deploy/static/js/main.827b3e3c.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "155256543dcede4dcdc70a4b81b2e75f",
+    "url": "/react-deploy/static/media/IMG_1048.15525654.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1b1a1826181565f9501bee47cf1224e.js"
+  "/react-deploy/precache-manifest.e577eeb469b361b68a94a7950b4db3a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
